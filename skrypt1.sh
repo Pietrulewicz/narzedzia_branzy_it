bbff0e12d843cc8ed5f4fde697c554822dea5045
Pietrulewicz <dsw45711@student.dsw.edu.pl>
2022-07-15 21:39:24 +0200

    ””

--- skrypt1.sh
+++ skrypt1.sh
@@ -2,4 +2,8 @@
 argument=$1
 if [[ "$argument" == "--date" ]]; then
     echo $(date)
+elif [[ "$argument" == "--logs" && "$2" ]]; then
+for ((x = 1 ; x < $2+1 ; x++)); do
+echo log$x $0 $(date) > log$x.txt
+done
 fi
